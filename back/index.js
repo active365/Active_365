dc1426c9f89ee7b2793639d093ee2d719d8edc56
aldairnunezca <aldairnunezca@gmail.com>
2024-12-07 10:25:49 -0600

    Function created

---
+++ back/index.js
@@ -0,0 +1,11 @@
+// function laCajaDePandora(numero){
+//     // proximamente escribiremos codigo aqui
+//     }
+
+function Carlos(){
+    return{
+        nombre: "Carlos",
+        edad: 30,
+        nacionalidad: "Mexicano"
+    }
+}
