71f363116e3cdfadd2cb2a61e54bedd5dec08519
C4nd3l-m <c4nd3l.m@gmail.com>
2024-12-07 13:20:16 -0300

    funcion persona

--- front/index.js
+++ front/index.js
@@ -1,7 +1,16 @@
-function laCajaDePandora(numero) {
+/*function laCajaDePandora(numero) {
     if (numero % 2 === 0) {
         return numero.toString(2);
     } else {
         return numero.toString(16);
+    }
+}*/
+
+
+const persona = () =>{
+    return {
+        Nombre: "Candela Villaverde",
+        Edad: 25,
+        Nacionalidad: "argentina"
     }
 }
