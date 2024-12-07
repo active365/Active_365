8ea5b05fc674998ff6bffd123afd8cf78784cf8a
FAgarcias <garciafergie997@gmail.com>
2024-12-07 11:27:11 -0500

    cambio 2

--- front/index.js
+++ front/index.js
@@ -1,4 +1,4 @@
-function CajaDePandora(numero) {
+/*function CajaDePandora(numero) {
     // Verificar si el número es par
     if (numero % 2 === 0) {
         return numero.toString(2); // Convertir a binario
@@ -6,4 +6,17 @@ function CajaDePandora(numero) {
         return `El número ${numero} no es par.`;
     }
 }
-console.log(CajaDePandora(10));
+console.log(CajaDePandora(10));*/
+
+
+
+function fergie() {
+    return {
+      nombre: "Fergie",
+      edad: 32,
+      nacionalidad: "Colombiana"
+    };
+  }
+  
+  // Llamar a la función
+  console.log(fergie());
