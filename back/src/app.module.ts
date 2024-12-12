d3f61199344fc5925f74b5ce9829d0bc21e45acb
Oloco <santillanfrancisco075@gmail.com>
2024-12-12 13:48:22 -0300

    Tidying up the app.module

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -2,14 +2,11 @@ import { Module } from '@nestjs/common';
 import typeorm from './config/db.config';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-<<<<<<< HEAD
 import { UserModule } from './users/user.module';
-=======
 import { GymsModule } from './gyms/gyms.module';
->>>>>>> origin/developer
-
 @Module({
-  imports: [UserModule,
+  imports: [GymsModule,
+    UserModule,
     ConfigModule.forRoot({
     isGlobal: true,
     load: [typeorm],
@@ -18,10 +15,6 @@ import { GymsModule } from './gyms/gyms.module';
     inject: [ConfigService],
     useFactory: (config: ConfigService) => config.get('typeorm'),
   }),
-<<<<<<< HEAD
   ]
-=======
-  GymsModule]
->>>>>>> origin/developer
 })
 export class AppModule {}
