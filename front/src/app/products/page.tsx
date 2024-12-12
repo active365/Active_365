ec9bb557bf85144bc9d1b0619b1993a56999a3e0
C4nd3l-m <c4nd3l.m@gmail.com>
2024-12-12 09:49:12 -0300

    "Creacion de products y card"

--- front/src/app/products/page.tsx
+++ front/src/app/products/page.tsx
@@ -1,6 +1,6 @@
 import Card from "@/components/productCard/Card";
 
-const Home: React.FC = () => {
+const Products: React.FC = () => {
     return (
         <div className="flex flex-col items-center justify-center min-h-screen">
             <h1 className="text-5xl font-semibold text-center text-white mb-8">
@@ -13,4 +13,4 @@ const Home: React.FC = () => {
     );
 }
 
-export default Home;
+export default Products;
