180b62e678daad857bfdd92dea1bd237bfa3ce02
Oloco <santillanfrancisco075@gmail.com>
2024-12-12 21:50:01 -0300

    Modifying user seeders to make the relationship with the gyms

--- back/src/users/user.module.ts
+++ back/src/users/user.module.ts
@@ -3,9 +3,11 @@ import { UserController } from './user.controller';
 import { UserService } from './user.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Users } from 'src/entities/users.entity';
+import { Gyms } from 'src/entities/gyms.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Users])],
+  imports: [TypeOrmModule.forFeature([Users]), 
+            TypeOrmModule.forFeature([Gyms])],
   controllers: [UserController],
   providers: [UserService]
 })
--- back/src/users/user.service.ts
+++ back/src/users/user.service.ts
@@ -4,9 +4,11 @@ import { Users } from 'src/entities/users.entity';
 import { Repository } from 'typeorm';
 import * as usersSeed from '../seeders/users.seeder.json';
 import * as bcrypt from 'bcrypt';
+import { Gyms } from 'src/entities/gyms.entity';
 @Injectable()
 export class UserService {
-    constructor(@InjectRepository(Users) private readonly userRepository: Repository<Users>) {}
+    constructor(@InjectRepository(Users) private readonly userRepository: Repository<Users>,
+                @InjectRepository(Gyms) private readonly gymsRepository: Repository<Gyms>) {}
 
     async getAllUsers(page: number, limit: number) {
         const users =  await this.userRepository.find();
@@ -28,8 +30,30 @@ export class UserService {
         await this.userRepository.update(id, user);
         return `El usuario con el id ${id} ha sido actualizado`;
       }
+
+      private async waitForGyms() {
+        const pollInterval = 500; // Intervalo de espera en milisegundos (0.5 segundos)
+        const timeout = 10000; // Tiempo máximo de espera en milisegundos (10 segundos)
+        let elapsedTime = 0;
+    
+        while (elapsedTime < timeout) {
+            const gymsCount = await this.gymsRepository.count();
+            if (gymsCount > 0) {
+                return; // Gimnasios encontrados, continuar con el flujo
+            }
+            await new Promise((resolve) => setTimeout(resolve, pollInterval)); // Espera activa
+            elapsedTime += pollInterval;
+        }
+    
+        throw new Error('Timeout: Gyms were not initialized in time.');
+    }
       async onModuleInit() {
+        await this.waitForGyms();
         const usersMock = await Promise.all(usersSeed.map(async (user) => {
+          const gyms = await this.gymsRepository.find();
+          console.log(gyms);
+          const gymForUser = await this.gymsRepository.findOne({ where: { city: user.city } });
+          console.log(gymForUser);
             const newUser = new Users();
             newUser.name = user.name;
             newUser.email = user.email;
@@ -40,20 +64,18 @@ export class UserService {
             newUser.height = user.height;
             newUser.weight = user.weight;
             newUser.password = await bcrypt.hash(user.password, 10);
+            newUser.gym = gymForUser ? gymForUser : null;
+            //console.log(newUser);
             return newUser;
           }));
-        for(const user of usersMock) {
-            const existingUser = await this.userRepository.findOne({where: {email: user.email}});
-            if(!existingUser) {
-                await this.userRepository
-                    .createQueryBuilder()
-                    .insert()
-                    .into(Users)
-                    .values({ name: user.name, email: user.email, phone: user.phone, address: user.address, city: user.city, rol: user.rol, height: user.height, weight: user.weight, password: user.password })
-                    .execute();
+          for (const user of usersMock) {
+            const existingUser = await this.userRepository.findOne({ where: { email: user.email } });
+            if (!existingUser) {
+                await this.userRepository.save(user);
             }
-      }
-      return 'Users added';
+        }
+    
+        return 'Users added';
     }
 
 }
