df5543d1273167eb6c81f99f92093e00649e028d
C4nd3l-m <c4nd3l.m@gmail.com>
2024-12-13 15:20:35 -0300

    new styles

--- front/src/app/products/page.tsx
+++ front/src/app/products/page.tsx
@@ -1,6 +1,15 @@
 import React from "react";
 import Card from "@/components/productsCard/Card";
-import { arrayProducts } from "@/helpers/arrayProducts"; // Ajusta la ruta según tu estructura de archivos
+import { arrayProducts } from "@/helpers/arrayProducts"; 
+import { categories } from "@/helpers/arrayProducts";
+
+type CategoryName = "Fitness Equipment" | "Yoga Accessories" | "Supplements";
+
+const categoryImages: Record<CategoryName, string> = {
+    "Fitness Equipment": "/Pesa.png",
+    "Yoga Accessories": "/mat.png",
+    "Supplements": "/supplement.png",
+};
 
 const Products: React.FC = () => {
     const currentDate = new Date();
@@ -9,16 +18,16 @@ const Products: React.FC = () => {
     return (
         <div className="flex flex-col items-center justify-center min-h-screen bg-black">
             {currentDate < deadline && (
-                <div
-                    className="relative text-white text-center w-full py-20"
-                    style={{
-                        backgroundImage: "url('/bannerSanta.png')",
-                        backgroundSize: "cover",
-                        backgroundPosition: "center",
-                        backgroundRepeat: "no-repeat",
-                        height: "400px",
-                    }}
-                >
+                <div className="relative text-white text-center w-full py-20">
+                    <video
+                        className="absolute inset-0 w-full h-full object-cover"
+                        autoPlay
+                        loop
+                        muted
+                    >
+                        <source src="/mostPopular.mp4" type="video/mp4" />
+                        Your browser does not support the video tag.
+                    </video>
                 </div>
             )}
 
@@ -26,7 +35,20 @@ const Products: React.FC = () => {
                 Everything for your favorite sports
             </h1>
 
-            {/* Pasa el array de productos al componente Card */}
+            {/* Sección de Categorías */}
+            <div className="flex justify-center space-x-10 mb-8">
+                {categories.map((category) => (
+                    <div key={category.id} className="flex flex-col items-center">
+                        <img
+                            src={categoryImages[category.name as CategoryName]}
+                            alt={category.name}
+                            className="w-20 h-20 object-contain mb-2"
+                        />
+                        <span className="text-white">{category.name}</span>
+                    </div>
+                ))}
+            </div>
+
             <div>
                 <Card products={arrayProducts} />
             </div>
