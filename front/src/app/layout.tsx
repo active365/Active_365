e3db07f7ececd09484b6a0e1ab3a3130fab45344
C4nd3l-m <c4nd3l.m@gmail.com>
2024-12-16 14:42:56 -0300

    fix layout2

--- front/src/app/layout.tsx
+++ front/src/app/layout.tsx
@@ -2,9 +2,6 @@
 import "@/app/globals.css";
 import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
-import SearchPage from "./search/page";
-import { CartProvider } from "@/context/CartContext"; 
-
 
 export default function RootLayout({
   children,
@@ -14,22 +11,15 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <CartProvider>
-          <Navbar />
-          {/* Contenedor para el buscador y el menú */}
-          <div className="flex items-center justify-center px-4 py-4 w-full max-w-screen-xl mx-auto">
-            {/* Contenedor de la búsqueda y el menú desplegable, apilados uno encima del otro */}
-            <div className="flex flex-col items-center space-y-4 w-full max-w-screen-xl">
-              {/* El buscador */}
-              <SearchPage />
-
-            </div>
+        <Navbar />
+        <div className="flex items-center justify-center px-4 py-4 w-full max-w-screen-xl mx-auto">
+          <div className="flex flex-col items-center space-y-4 w-full max-w-screen-xl">
+            {/* Aquí ya no se incluye SearchPage */}
+            {children}
           </div>
+        </div>
 
-          <main>{children}</main>
-          <Footer />
-        </CartProvider>
-
+        <Footer />
       </body>
     </html>
   );
