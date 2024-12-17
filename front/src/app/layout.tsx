9f633cf93cc894bb2e963f1b8e0f114b60926cd1
MilagrosLuque <luquemariamilagros@gmail.com>
2024-12-17 11:15:31 -0300

    fix/ demo 1

--- front/src/app/layout.tsx
+++ front/src/app/layout.tsx
@@ -2,6 +2,7 @@
 import "@/app/globals.css";
 import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
+import { CartProvider } from "@/context/CartContext"; 
 
 
 export default function RootLayout({
@@ -14,19 +15,10 @@ export default function RootLayout({
       <body>
         <CartProvider>
           <Navbar />
-          {/* Contenedor para el buscador y el menú */}
-          <div className="flex items-center justify-center px-4 py-4 w-full max-w-screen-xl mx-auto">
-            {/* Contenedor de la búsqueda y el menú desplegable, apilados uno encima del otro */}
-            <div className="flex flex-col items-center space-y-4 w-full max-w-screen-xl">
-              {/* El buscador */}
-            
-
-            </div>
-
-          </div>
-        </div>
-
+          
+          <main>{children}</main>
         <Footer />
+        </CartProvider>
       </body>
     </html>
   );
