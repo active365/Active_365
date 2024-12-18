efc9d88c5848c7bbf356e7838034b0bf215365f2
C4nd3l-m <c4nd3l.m@gmail.com>
2024-12-18 16:05:09 -0300

    fix detailpage

--- front/src/app/product/[id]/page.tsx
+++ front/src/app/product/[id]/page.tsx
@@ -1,3 +1,4 @@
+"use client"
 import React, { useState, useEffect, useContext } from "react";
 import { GeneralContext } from "@/context/GeneralContext"; 
 import DetailCard from "@/components/detailCard/DetailCard";
