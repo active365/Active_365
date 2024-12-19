8fe63958e25167a1c0d8aa8ede6be36e28b6e9c3
aldairnunezca <aldairnunezca@gmail.com>
2024-12-19 16:07:20 -0600

    feat/Stripe fully functioning

--- back/src/stripe/stripe.controller.ts
+++ back/src/stripe/stripe.controller.ts
@@ -1,12 +1,27 @@
-import { Body, Controller, Get, Param, ParseUUIDPipe, Post } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  ParseUUIDPipe,
+} from '@nestjs/common';
 import { StripeService } from './stripe.service';
 
-@Controller('stripe')
+@Controller('checkout')
 export class StripeController {
   constructor(private readonly stripeService: StripeService) {}
 
-  @Get()
-  async getCheckoutSession() {
-    return this.stripeService.getCheckoutSession();
+  @Get('/success')
+  async successCheckout() {
+    return this.stripeService.successCheckout();
+  }
+
+  @Get('/cancelled')
+  async cancelCheckout() {
+    return this.stripeService.cancelCheckout();
+  }
+
+  @Get('/:orderId')
+  async getCheckoutSession(@Param('orderId', ParseUUIDPipe) orderId: string) {
+    return this.stripeService.getCheckoutSession(orderId);
   }
 }
--- back/src/stripe/stripe.service.ts
+++ back/src/stripe/stripe.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { OrderDetails } from 'src/entities/orderDetails.entity';
 import { Orders } from 'src/entities/orders.entity';
@@ -16,36 +16,42 @@ export class StripeService {
     @InjectRepository(Users)
     private usersRepository: Repository<Users>,
   ) {}
-  async getCheckoutSession() {
+
+  async getCheckoutSession(orderId: string) {
     const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
+   const order = await this.ordersRepository.findOne({
+    where: { id: orderId },
+    relations: ['orderDetails', 'orderDetails.orderProducts', 'orderDetails.orderProducts.product'],
+   })
+    if (!order) {
+      throw new NotFoundException(`Order with id ${orderId} not found`);
+    }
+
+    const lineItems = order.orderDetails.orderProducts.map((orderProduct) => ({
+      price_data:{
+        currency: 'usd',
+        product_data: {
+          name: orderProduct.product.name,
+        },
+        unit_amount: orderProduct.product.price * 100,
+      },
+      quantity: orderProduct.quantity,
+    }));
+    
     return await stripe.checkout.sessions.create({
-      line_items: [
-        {
-          price_data: {
-            product_data: {
-              name: 'Laptop',
-            },
-            currency: 'usd',
-            unit_amount: 10 * 100,
-          },
-          quantity: 1,
-        },
-        {
-          price_data: {
-            product_data: {
-              name: 'Cellphone',
-            },
-            currency: 'usd',
-            unit_amount: 20 * 100,
-          },
-          quantity: 2,
-        },
-      ],
+      line_items: lineItems,
       mode: 'payment',
       payment_method_types: ['card'],
-      success_url: `${process.env.CLIENT_URL}`,
-      cancel_url: `${process.env.CLIENT_URL}`
+      success_url: `${process.env.CLIENT_URL}/checkout/success`,
+      cancel_url: `${process.env.CLIENT_URL}/checkout/cancelled`,
     });
   }
+
+  async successCheckout() {
+    return 'Purchase successful';
+  }
+  async cancelCheckout() {  
+    return 'Purchase cancelled';
+  } 
 }
