a4351bf561912937f1998d6d9c2c9aad6d9f7e55
aldairnunezca <aldairnunezca@gmail.com>
2025-01-03 10:38:07 -0600

    feat: swagger integrated

--- back/src/main.ts
+++ back/src/main.ts
@@ -1,10 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe())
+
+  const swaggerConfig = new DocumentBuilder()
+                            .setTitle('DEMO Active365')
+                            .setDescription('API for Active365')
+                            .setVersion('1.0')
+                            .build();
+ const document = SwaggerModule.createDocument(app, swaggerConfig);                           
+  SwaggerModule.setup('api', app, document);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
